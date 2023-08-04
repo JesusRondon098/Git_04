c3d83310bd810c81f570011d85389ed484221ffa
JesusRondon098 <rondonbautista1@gmail.com>
2023-08-04 11:24:45 +0200

    reto5

--- run.js
+++ run.js
@@ -2,7 +2,7 @@ let myLib = require("./index.js");
 //
 console.log("*****Reto2.2****");
 //
-console.log(myLib.sumar(3,5));
+console.log(myLib.sumar(3,5,7));
 console.log(myLib.restar(3,5));
 console.log(myLib.producto(3,5));
 console.log(myLib.dividir(3,5));
